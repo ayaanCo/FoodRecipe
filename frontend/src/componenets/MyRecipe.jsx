25809446b6c25c4c9209a9e0742ed30e84420bfd
ayaanCo <ayaanshaikh2422@gmail.com>
2025-08-06 07:56:13 +0530

    Update MyRecipe.jsx

--- frontend/src/componenets/MyRecipe.jsx
+++ frontend/src/componenets/MyRecipe.jsx
@@ -10,7 +10,7 @@ const [Recipes, setRecipes] = useState([]);
 
       const handleFetchRecipes = async() => {  
           try {
-              const response =await fetch('http://localhost:5000/recipes'); // Replace with your API endpoint
+              const response =await fetch('https://foodrecipe-rug5.onrender.com/recipes'); // Replace with your API endpoint
               if (!response.ok) { 
                   throw new Error('Network response was not ok');
               }
